37b116531a856e38992e3a8537c33e9606254446
148rashunta <ishunta148raw01f826@gmail.com>
2021-05-25 17:19:23 +0900

    ishihara coded halfway of ecall_start function in enclave.cpp

--- server/Enclave/enclave.cpp
+++ server/Enclave/enclave.cpp
@@ -1,1 +1,8 @@
-#include <stdio.h>
+#include "Enclave_t.h"
+#include <sgx_trts.h>
+
+int ecall_start(KeyValue data, KeyValue table[][], long seed)
+{
+    stash = cuckoo(data, table, seed, 0, 0, 5);
+    return 1;
+}
