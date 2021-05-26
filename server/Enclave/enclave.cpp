e2e902064e0be8bc9187755ed4017b91566a80a9
石原詢大 <ishihara@ishiharajundainoMacBook-Pro.local>
2021-05-26 18:07:29 +0900

    ishihara modified enclave.cpp little

--- server/Enclave/enclave.cpp
+++ server/Enclave/enclave.cpp
@@ -1,8 +1,13 @@
 #include "Enclave_t.h"
 #include <sgx_trts.h>
 
-int ecall_start(KeyValue data, KeyValue table[][], long seed)
+template <typename KeyValue>
+int ecall_start(KeyValue data, KeyValue table[][], int seed, int size)
 {
+    int rnd;
     stash = cuckoo(data, table, seed, 0, 0, 5);
+
+    rnd = rand % size;
+    stash 
     return 1;
 }
