70fab1077c296de30ab9a4cd2bf0a53f6c0cffdc
石原詢大 <ishihara@ishiharajundainoMacBook-Pro.local>
2021-05-27 21:13:51 +0900

    ishihara coded the template of enclave.cpp

--- server/Enclave/enclave.cpp
+++ server/Enclave/enclave.cpp
@@ -1,13 +1,32 @@
-#include "Enclave_t.h"
-#include <sgx_trts.h>
+// #include "Enclave_t.h"
+// #include <sgx_trts.h>
+#include <random>
+#include "classes.h"
 
-template <typename KeyValue>
-int ecall_start(KeyValue data, KeyValue table[][], int seed, int size)
+template <class KeyValue> int ecall_start(KeyValue data, KeyValue table[][], int size)
 {
-    int rnd;
-    stash = cuckoo(data, table, seed, 0, 0, 5);
+    std::random_device rnd;
+    stash.put(cuckoo(data, table, size, 0, 0, 5));
+    
+    std::string key = "dummy_";
+    unsigned int r = rnd();
+    key += std::to_string(r);
+    KeyValue dummy(key);
+    stash.put(cuckoo(dummy, table, size, 0, 0, 5));
 
-    rnd = rand % size;
-    stash 
+    ocall_return_stash();
     return 1;
+}
+
+template <class KeyValue> KeyValue cuckoo(KeyValue data, KeyValue[][] table, int size, int tableID, int cnt, int limit)
+{
+    if (cnt == limit) return data;
+
+    int pos[2];
+    pos[0] = hash_1(decrypt(data.getKey()), size);
+    pos[1] = hash_2(decrypt(data.getKey()), size);
+
+    KeyValue w = table[tableID][pos[tableID]];
+    table[tableID][pos[tableID]] = data;
+    return cuckoo(w, table, size, (tableID+1)%2, cnt+1, limit);
 }
